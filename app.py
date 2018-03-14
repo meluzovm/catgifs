c03c86e6100e18348e79060749f35416139ee4e0
Lee Gaines <eightlimbed@gmail.com>
2018-03-14 11:08:59 -0700

    adds local images

--- app.py
+++ app.py
@@ -7,18 +7,20 @@ app = Flask(__name__)
 
 # a bunch of cat gifs
 images = [
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26388-1381844103-11.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr01/15/9/anigif_enhanced-buzz-31540-1381844535-8.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26390-1381844163-18.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/10/anigif_enhanced-buzz-1376-1381846217-0.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr03/15/9/anigif_enhanced-buzz-3391-1381844336-26.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/10/anigif_enhanced-buzz-29111-1381845968-0.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr03/15/9/anigif_enhanced-buzz-3409-1381844582-13.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr02/15/9/anigif_enhanced-buzz-19667-1381844937-10.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26358-1381845043-13.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/9/anigif_enhanced-buzz-18774-1381844645-6.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr06/15/9/anigif_enhanced-buzz-25158-1381844793-0.gif",
-    "http://ak-hdl.buzzfed.com/static/2013-10/enhanced/webdr03/15/10/anigif_enhanced-buzz-11980-1381846269-1.gif"
+    "./static/images/cat-0.gif",
+    "./static/images/cat-1.gif",
+    "./static/images/cat-2.gif",
+    "./static/images/cat-3.gif",
+    "./static/images/cat-4.gif",
+    "./static/images/cat-5.gif",
+    "./static/images/cat-6.gif",
+    "./static/images/cat-7.gif",
+    "./static/images/cat-8.gif",
+    "./static/images/cat-9.gif",
+    "./static/images/cat-10.gif",
+    "./static/images/cat-11.gif",
+    "./static/images/cat-12.gif",
+    "./static/images/cat-13.gif"
 ]
 
 @app.route('/')
